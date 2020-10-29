b24c509eb7710b7bd8fa5d19fff721af36a84fee
ChackaFry <72393147+ChackaFry@users.noreply.github.com>
2020-10-29 12:46:06 +0000

    Add files via upload

--- student_selector.py
+++ student_selector.py
@@ -7,43 +7,53 @@ import zipfile
 def find_files(files, filetype):
     """
     Finds all files of a specific file type in the current directory
-    :param files: List of string containg all files in the directory
+    :param files: List of string containing all files in the directory
     :param filetype: string containing the filetype to be searched for. e.g. ".zip"
     :return: Lis of string containing all files of that filetype
     """
-    zip_files = list()
-    for file in files:
-        if file[-1 * len(filetype):] == filetype:
-            zip_files.append(file)
-    return zip_files
+    files_of_type = list()
+    for f in files:
+        if f[-1 * len(filetype):] == filetype:
+            files_of_type.append(f)
+    return files_of_type
 
 
-def select_random(list_of_things):
+def select_random(list_of_things, copy):
     """
-    Selects a random element from the list
+    Selects a random element from the list, then removes it from the list
     :param list_of_things: List of any type
+    :param copy: List of any type, containing all the values of list_of_things
     :return: random element from the List
     """
-    return list_of_things[rnd.randrange(0, len(list_of_things))]
+    # Adding the removed elements back into the list
+    if len(list_of_things) == 0:
+        for element in copy:
+            list_of_things.append(element)
+
+    value = rnd.randrange(0, len(list_of_things))  # Random element chosen
+    selected_element = list_of_things[value]
+    list_of_things.pop(value)  # Removing selected element
+    return selected_element
 
 
-def new_student(canvas, x, y, text, color, font_size):
+def new_student(canvas, x_co, y_co, text, color, f_size):
     """
     Resets the Canvas, to display a new student
     :param canvas: Canvas to modify
-    :param x: int x coordinate of centre of the text
-    :param y: int y coordinate of centre of the text
+    :param x_co: int x coordinate of centre of the text
+    :param y_co: int y coordinate of centre of the text
     :param text: string of text to be input into canvas
     :param color: string containing colour info in hex, e.g. "#0f1a2b"
-    :param font_size: int representing the font size of the text
+    :param f_size: int representing the font size of the text
     :return: Canvas that has been updated.
     """
     canvas.delete("all")
-    canvas.create_text(x, y,
+    canvas.create_text(x_co, y_co,
                        fill=color,
-                       font=f"Calibri {font_size} italic bold",
+                       font=f"Calibri {f_size} italic bold",
                        text=text)
     canvas.pack()
+
 
 # Find and Extract all ZIP files in the current dir
 zip_files = find_files(os.listdir(), ".zip")
@@ -66,17 +76,23 @@ for file in csv_files:
             elements.pop(0)  # Remove timestamp
             new_file += f"{elements[0]:}: {elements[1]}\n"
             new_file = new_file.replace("\"", "")  # Remove '"' char from the string
-    os.remove(file)  # Deleteing old file
+    os.remove(file)  # Deleting old file
     new = open(file, "w")
     new.write(new_file)
     new.close()
 
 x = open(csv_files[0], "r")
-studentARR = x.read().split("\n")  # Getting Student name from the new file
+student_names = x.read().split("\n")  # Getting Student name from the new file
 x.close()
+student_names.remove("")
+
+# Creating a copy of studentARR
+student_names_copy = []
+for stu in student_names:
+    student_names_copy.append(stu)
 
 maxLength = 0
-for student in studentARR:
+for student in student_names:
     if len(student) > maxLength:
         maxLength = len(student)
 
@@ -93,11 +109,12 @@ red = 100
 green = 100
 blue = 200
 
-can = Canvas(root, width=width, height=height, bg='#000000')
+canv = Canvas(root, width=width, height=height, bg='#000000')
 btn_student = Button(root, text="New Student",
-                     command=lambda: new_student(can, xC, yC, select_random(studentARR),
+                     command=lambda: new_student(canv, xC, yC,
+                                                 select_random(student_names, student_names_copy),
                                                  f"#{red:0>2x}{green:0>2x}{blue:0>2x}", font_size))
-can.pack()
+canv.pack()
 btn_student.pack()
 
 root.mainloop()
